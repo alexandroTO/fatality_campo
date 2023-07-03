f26aecb223a82a8f5e0919cc6ae366f9fb760d47
Alexandro Tiago de Oliveira <48971595+alexandroTO@users.noreply.github.com>
2023-07-03 11:35:52 -0300

    Corrigido layout dos componentes header e menu

--- php/menu.php
+++ php/menu.php
@@ -1,5 +1,18 @@
-<ul class="d-flex justify-content-center mEspaco nav nav text-white">
-    <li>HOME</li>
-    <li>CADASTRE SUA EQUIPE</li>
-    <li>AGENDA</li>
-</ul>
+<nav class="navbar navbar-expand-lg mEspaco " >
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse mEspacoItem d-flex justify-content-center">
+            <ul class="navbar-nav ">
+                <li class="nav-item">
+                    <a class="nav-link" href="">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Cadastre sua equipe</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Agenda</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
